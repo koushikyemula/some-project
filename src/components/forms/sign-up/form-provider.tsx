35631c10f1bdab3941ef6e9b0378c067adf1bcf6
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-06-04 23:28:53 +0530

    forms component and use sign up hook

---
+++ src/components/forms/sign-up/form-provider.tsx
@@ -0,0 +1,11 @@
+import { AuthContextProvider } from "@/context/use-auth-context";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const SignUpFormProvider = ({ children }: Props) => {
+  // return <AuthContextProvider>{/* <formprovider */}</AuthContextProvider>;
+};
+
+export default SignUpFormProvider;
